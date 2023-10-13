cc17b80d70e4a2541e73a8f4badc8fd18e3d1c90
Teemu Koivisto <tkoivisto456@gmail.com>
2023-10-13 12:39:40 +0300

    initial commit

---
+++ packages/client/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Editor from './Editor.svelte'
+
+  import '../tailwind.css'
+</script>
+
+<section class="my-16 flex flex-col items-center">
+  <h1 class="mt-12 text-3xl hover:underline">
+    <a href="https://teemukoivisto.github.io/prosemirror-node-view-gutters/">
+      prosemirror-node-view-gutters
+    </a>
+  </h1>
+  <p class="my-8 hover:underline">
+    <a href="https://github.com/TeemuKoivisto/prosemirror-node-view-gutters">GitHub repo</a>
+  </p>
+  <Editor />
+</section>
+
+<style lang="scss">
+</style>
